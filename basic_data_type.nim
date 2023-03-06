46facbe34ad1a26ba522c210d3e2af725dd79e9f
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-03-06 15:37:01 +0700

    Bassic data type exercise
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- basic_data_type.nim
+++ basic_data_type.nim
@@ -39,9 +39,11 @@ const
 
 # 1 day is 24 hour
 # 30 day is 24 * 30 hour
+
+# bob 1 day is (3.14 * 8)
 let
   alice_after_30day:float = alice_earn_every_15 * 2
-  bob_after_30day:float = bob_earn_perhour * 30
+  bob_after_30day:float = (bob_earn_perhour * 8) * 30
   isAlice_earned_more_than_Bob:bool = alice_after_30day > bob_after_30day
 
 echo "is Alice earned more than Bob? ", isAlice_earned_more_than_Bob
